63412de2d498e18e2f4ee2a5b1409f4a16219942
Matt Tarbit <mtarbit@gmail.com>
2013-11-01 12:41:18 +0000

    Update both expanded & compress CSS with Sass watch script.

--- _scripts/sass.sh
+++ _scripts/sass.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
+# Run expanded & compressed Sass watch processes side-by-side,
+# intermingling their output. Ctrl-C twice to quit both Sasses.
+
+# Enable job control so we can foreground the backgrounded
+# 'expanded' sass process and send it a Ctrl-C after we've
+# interrupted the foregrounded 'compressed' sass process.
+
+# I tried cleaning up with kill instead, sending various
+# signals to the background process's PID, but to no avail.
+# The background process quits, but it leaves an orphaned
+# watch process behind:
+
+set -m
+
 src_dir="$(dirname "${BASH_SOURCE[0]}")/.."
 css_dir="$src_dir/assets/stylesheets"
 
-# sass --style expanded --watch "$css_dir/_sass/main.scss":"$css_dir/main.css"
+sass --style expanded --watch "$css_dir/_sass/main.scss":"$css_dir/main.css" &
 sass --style compressed --watch "$css_dir/_sass/main.scss":"$css_dir/main.min.css"
+
+fg 1
+
